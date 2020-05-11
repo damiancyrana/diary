af4979978cf573256a3a527625315310a088ee54
Damian Cyrana <damiancyrana@gmail.com>
2020-05-11 12:41:04 +0200

    Added subpages

--- diary/urls.py
+++ diary/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'diary'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('subjects/', views.subjects, name='subjects'),
+    re_path(r'^subjects/(?P<subject_id>\d+)/$', views.subject, name='subject'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Subject
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def subjects(request):
+    subjects = Subject.objects.order_by('addition_time')
+    context = {'subjects': subjects}
+    return render(request, 'subject/subjects.html', context)
+
+
+def subject(request, subject_id):
+    subject = Subject.objects.get(id=subject_id)
+    entries = subject.entry_set.order_by('-addition_time')
+    context = {'subject': subject, 'entries': entries}
+    return render(request, 'subject/subject.html', context)
