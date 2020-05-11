417987cd762414a05793f9f0a758296b499bc9ab
Damian Cyrana <damiancyrana@gmail.com>
2020-05-11 15:56:39 +0200

    Separating users

--- diary/models.py
+++ diary/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Subject(models.Model):
     text = models.CharField(max_length=200)
     addition_time = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
--- diary/views.py
+++ diary/views.py
@@ -1,40 +1,50 @@
 from django.shortcuts import render
-from .models import Subject
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from .forms import SubjectForm, EntryForm
 from .models import Subject, Entry
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'index.html')
 
 
+@login_required
 def subjects(request):
-    subjects = Subject.objects.order_by('addition_time')
+    subjects = Subject.objects.filter(owner=request.user).order_by('addition_time')
     context = {'subjects': subjects}
     return render(request, 'subject/subjects.html', context)
 
 
+@login_required
 def subject(request, subject_id):
     subject = Subject.objects.get(id=subject_id)
+    if subject.owner != request.user:
+        raise Http404
+
     entries = subject.entry_set.order_by('-addition_time')
     context = {'subject': subject, 'entries': entries}
     return render(request, 'subject/subject.html', context)
 
 
+@login_required
 def new_subject(request):
     if request.method != 'POST':
         form = SubjectForm()
     else:
         form = SubjectForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_subject = form.save(commit=False)
+            new_subject.owner = request.user
+            new_subject.save()
             return HttpResponseRedirect(reverse('diary:subjects'))
+
     contex = {'form': form}
     return render(request, 'subject/new_subject.html', contex)
 
 
+@login_required
 def new_entry(request, subject_id):
     subject = Subject.objects.get(id=subject_id)
     if request.method != 'POST':
@@ -50,9 +60,13 @@ def new_entry(request, subject_id):
     return render(request, 'subject/new_subject.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     subject = entry.subject
+    if subject.owner != request.user:
+        raise Http404
+
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
@@ -60,5 +74,6 @@ def edit_entry(request, entry_id):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('diary:subject', args=[subject.id]))
+
     context = {'entry': entry, 'subject': subject, 'form': form}
     return render(request, 'entry/edit_entry.html', context)
